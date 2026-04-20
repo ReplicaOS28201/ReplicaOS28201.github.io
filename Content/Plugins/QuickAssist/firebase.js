6e4e68401311693d15bdae54fa2bfbea9f7e1a2e
ShadowDev <teammicro2026@outlook.com>
2026-04-20 08:55:34 +0200

    Update firebase.js

--- Content/Plugins/QuickAssist/firebase.js
+++ Content/Plugins/QuickAssist/firebase.js
@@ -2,9 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyClsOvXFunSyZXOKNIEGFTM8etLw6lQgH4",
+  authDomain: "os67z9zp.firebaseapp.com",
+  databaseURL: "https://os67z9zp-default-rtdb.firebaseio.com",
+  projectId: "os67z9zp",
+  storageBucket: "os67z9zp.firebasestorage.app",
+  messagingSenderId: "864031752567",
+  appId: "1:864031752567:web:4be14d5cab7d162c5ce172",
+  measurementId: "G-GRYPDQFHTY"
 };
 
 const app = initializeApp(firebaseConfig);
